5807680f07918fe2d4d92d32d896626dc583e9e4
Robert Yang <yang.robert.w@gmail.com>
2017-10-24 20:26:03 -0400

    week08, intro to proc gen: GrassManager.cs, TenPrint.cs

---
+++ week08_procgen/Assets/scripts/GrassManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassManager : MonoBehaviour {
+
+	public GameObject grassPrefab; // assign in Inspector
+	int grassCount = 0; // track how much grass we've made, so that we don't make too much
+
+	// Update is called once per frame
+	void Update () {
+		// if there's less than 500 blades of grass, then keep planting grass
+		if( grassCount < 500 ) {
+			// STEP 1: calculate where to clone / instantiate something
+			Vector3 randomPosition = new Vector3( Random.Range(-5f, 5f), 0f, Random.Range(-5f, 5f) );
+			// STEP 2: instantiate it at "randomPosition" and at rotation 0,0,0 and parent under GrassManager
+			Instantiate( grassPrefab, randomPosition, Quaternion.Euler(0,0,0), transform );
+
+			// increment counter so that we don't have infinite grass
+			grassCount++;
+		}
+	}
+}
---
+++ week08_procgen/Assets/scripts/TenPrint.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TenPrint : MonoBehaviour {
+
+	// set of random colors defined in Inspector
+	public Color[] randomColorPalette;
+
+	// instead of GameObject, I'm instantiating based on Transform instead
+	public Transform linePrefab; // assign in Inspector
+
+	// Use this for initialization
+	void Start () {
+		// use 2 for() loops to instantiate the linePrefab in a grid pattern
+		for( int y = 0; y < 100; y++ ) { // handle vertical increments; move down a row
+			for( int x = 0; x < 100; x++ ) { // handle horizontal increments; fill in the row
+				// STEP 1: decide where to put the clone
+				Vector3 spawnPos = new Vector3( x * 5f, y * 5f, 0f ); // grid size = 5f
+				// STEP 2: decide the clone's rotation too
+				Vector3 randomEuler = Vector3.zero;
+				if( Random.Range( 0, 100 ) > 50 ) { 
+					randomEuler = new Vector3( 0f, 0f, 45f ); // 50% chance to rotate forwards
+				} else { 
+					randomEuler = new Vector3( 0f, 0f, -45f ); // 50% chance to rotate backwards
+				}
+				// STEP 3: instantiate!
+				Transform newClone = (Transform)Instantiate( linePrefab, spawnPos, Quaternion.Euler( randomEuler ) );
+				newClone.localScale *= Random.Range( 0.5f, 2f ); // randomly resize the line segment
+				Color randomColor = randomColorPalette[Random.Range(0, randomColorPalette.Length)];
+				newClone.GetComponent<Renderer>().material.color = randomColor; // random color
+			}
+		}
+
+	}
+
+}
