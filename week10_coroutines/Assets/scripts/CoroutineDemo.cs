16b493cf430e237f640006197ff320c430023c40
Robert Yang <yang.robert.w@gmail.com>
2017-11-07 21:36:50 -0500

    week10a: CoroutineDemo.cs, MoveCoroutine.cs

---
+++ week10_coroutines/Assets/scripts/CoroutineDemo.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoroutineDemo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// start the coroutine
+		// MyFirstCoroutine(); // WRONG WAY, WILL NOT WORK
+		StartCoroutine( MyFirstCoroutine() ); // RIGHT WAY, WILL WORK
+
+	}
+	
+	// coroutine must return "IEnumerator" (NOT "IEnumerable" nor "Coroutine")
+	IEnumerator MyFirstCoroutine() {
+		Debug.Log( "coroutine started!" );
+		yield return 0; // pause or wait for 1 frame before continuing
+
+		Debug.Log( "waited 1 frame! now continuing..." );
+		yield return 0; // wait for 1 frame
+		yield return null; // wait for a second frame
+
+		Debug.Log( "ok, waited for 2 more frames, now continuing..." );
+		yield return new WaitForSeconds( 1f ); // wait for 1 second
+
+		Debug.Log( "ok, waited for 1 second, now continuing..." );
+
+		// example: how to do something during a duration
+		float t = 0f; // track how long we've been rotating the cube so far
+		while( t < 4.20f ) {
+			t += Time.deltaTime; // keep adding frame duration to "t"... after 4.2 s, t = 4.2
+			transform.Rotate( 0f, 90f * Time.deltaTime, 0f ); // rotate
+			yield return 0; // wait for a frame before continuing
+		}
+
+		// when the coroutine reaches the end... it ends
+		Debug.Log( "coroutine ended!" );
+
+	}
+
+
+}
---
+++ week10_coroutines/Assets/scripts/MoveCoroutine.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// another example: use a Coroutine to move a Sphere for [X] amount of seconds
+public class MoveCoroutine : MonoBehaviour {
+
+	bool isCoroutineRunning = false; // so that we don't let coroutines stack on top of each other
+	
+	// Update is called once per frame
+	void Update () {
+		// if you press SPACEBAR, then move the sphere for 5 seconds
+		if( Input.GetKeyDown( KeyCode.Space ) ) {
+			StartCoroutine( MoveSphereCoroutine( 5f ) );
+		}
+	}
+
+	IEnumerator MoveSphereCoroutine (float durationOfSeconds) {
+		Debug.Log( "starting move coroutine at " + Time.time.ToString() );
+
+		if( isCoroutineRunning == true ) { // is another coroutine already running?
+			yield break; // ends the coroutine early / immediately
+		}
+
+		isCoroutineRunning = true; // coroutine started, set this to TRUE
+
+		// similar pattern as in CoroutineDemo.cs; do ___ for a duration of time
+		for( float t = 0f; t < durationOfSeconds; t += Time.deltaTime ) {
+			transform.Translate( 0f, 0f, Time.deltaTime * 5f ); // move sphere
+			yield return 0; // VERY IMPORTANT: tell the coroutine to wait for a frame before continuing
+		}
+
+		isCoroutineRunning = false; // coroutine finished, set it back to false
+	}
+
+
+}
