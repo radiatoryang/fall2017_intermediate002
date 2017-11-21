46fc6ac56806c46429c2f35fd5a813fe3a6ae91f
Robert Yang <yang.robert.w@gmail.com>
2017-11-21 16:23:05 -0500

    week12: game feel, tweening -- see BlockTween.cs and Screenshake.cs

---
+++ week12_tweeningGameFeel/Assets/scripts/BlockTween.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockTween : MonoBehaviour {
+
+	Vector3 startPosition;
+	public Vector3 endPosition = new Vector3(0f, -5f, 0f );
+
+	public AnimationCurve tweeningCurve; // visual drawing of the tweening equation
+
+	void Awake () { // Awake() is like another "Start", but happens before Start()
+		startPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// DEBUG TEST: start the coroutine when I press SPACE
+		if( Input.GetKeyDown( KeyCode.Space ) ) {
+			StartCoroutine( TweenCoroutine() );
+		}
+	}
+
+	IEnumerator TweenCoroutine () {
+		// keep a Time counter, starting at 0; as long as Time < 1f, keep repeating; add frame duration to Time
+		for( float time = 0f; time < 1f; time += Time.deltaTime ) {
+			// move the cube from startPosition to endPosition based on time (0% - 100%)
+		 // transform.position = Vector3.Lerp( startPosition, endPosition, time );
+			transform.position = Vector3.LerpUnclamped( startPosition, endPosition, tweeningCurve.Evaluate(time) );
+			yield return 0;
+		}
+	}
+
+}
---
+++ week12_tweeningGameFeel/Assets/scripts/Screenshake.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// put this script on the Main Camera
+// but make sure the Main Camera is parented to an empty gameobject (a "Camera Rig"?)
+public class Screenshake : MonoBehaviour {
+
+	float shakeStrength = 0f; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// DEBUG TEST: if I press SPACE, then shake the screen
+		if( Input.GetKeyDown( KeyCode.Space ) ) {
+			shakeStrength = 1f;
+		}
+
+		// after that, let's look at shakeStrength and shake screen based on its value
+
+		// STEP 1: calculate a shakeOffset direction 
+		// "17f" == frequency, 0.25f == amplitude
+		Vector3 shakeOffset = Vector3.right * Mathf.Sin( Time.time * 37f ) * 0.25f; // left-right motion
+		shakeOffset += Vector3.up * Mathf.Sin( Time.time * 49f ) * 0.25f; // up-down motion
+
+		// STEP 2: let's apply the shakeOffset to our camera
+		transform.localPosition = shakeOffset * shakeStrength;
+
+		// STEP 3: decay the shakeStrength timer... but clamp it so it doesn't go into negative values
+		shakeStrength = Mathf.Clamp( shakeStrength - Time.deltaTime, 0f, 10f );
+	}
+}
